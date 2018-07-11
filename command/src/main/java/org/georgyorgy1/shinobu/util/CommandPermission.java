9100afeb1dfdde41c3fb93a8dd9421a7749d4bff
georgyorgy1 <georgesorsisafaggot@muslim.com>
2018-07-11 19:21:30 +0800

    Let's try this

---
+++ command/src/main/java/org/georgyorgy1/shinobu/util/CommandPermission.java
@@ -0,0 +1,11 @@
+package org.georgyorgy1.shinobu.util;
+
+import net.dv8tion.jda.core.entities.MessageChannel;
+
+public class CommandPermission
+{
+    public boolean isChannelAllowed(MessageChannel channel)
+    {
+        return false;
+    }
+}
